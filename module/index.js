2e7423fa6f381e49a51f2f0d2da67176bb2a79e9
Dragon_YT <92475365+TRASH-Dragon-YT@users.noreply.github.com>
2022-05-25 08:24:41 +0200

    Updated

--- module/index.js
+++ module/index.js
@@ -1,11 +1,11 @@
-const  Client  = require("./Client/index")
-const Intents = require("./Constants/intents")
-const DiscordEmbed = require('./Client/stuff/embed')
-const DiscordButton = require('./Client/stuff/button')
-const SlashCommands = require('./Client/stuff/slashcmds')
-const DiscordMenu = require('./Client/stuff/menu')
-const CommandHandler = require('./Client/stuff/CommandHandler')
-const ActivityTypes = require('./Client/stuff/activitytypes.js')
+const  Client  = require("./modules/Client/index")
+const Intents = require("./modules/Constants/intents")
+const DiscordEmbed = require('./modules/lient/stuff/embed')
+const DiscordButton = require('./modules/Client/stuff/button')
+const SlashCommands = require('./modules/Client/stuff/slashcmds')
+const DiscordMenu = require('./modules/Client/stuff/menu')
+const CommandHandler = require('./modules/Client/stuff/CommandHandler')
+const ActivityTypes = require('./modules/Client/stuff/activitytypes.js')
 
 module.exports = {
     Client,
