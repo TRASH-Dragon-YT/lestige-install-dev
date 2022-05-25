4e885a5eeffedcfa73d96e6d5d1c68f2fa50c29f
Dragon_YT <92475365+TRASH-Dragon-YT@users.noreply.github.com>
2022-05-25 08:33:47 +0200

    Patched errors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const  Client  = require("./module/Client/index")
 const Intents = require("./module/Constants/intents")
-const DiscordEmbed = require('./module/lient/stuff/embed')
+const DiscordEmbed = require('./module/Client/stuff/embed')
 const DiscordButton = require('./module/Client/stuff/button')
 const SlashCommands = require('./module/Client/stuff/slashcmds')
 const DiscordMenu = require('./module/Client/stuff/menu')
